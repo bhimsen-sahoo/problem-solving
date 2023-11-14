c32ba8568b8a897f58bd6d51f973ae8f1d77c994
bhimsen <bhimsen@railsfactory.org>
2023-11-14 13:05:50 +0530

    Day2 problem solved

---
+++ yr2023/day2/two_sum.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+  nums.each.with_index do |val1, i|
+    offset = i + 1
+    nums[offset..-1].each.with_index do |val2, j|
+      return [i, j + offset] if val1 + val2 == target
+    end
+  end
+
+  []
+end
